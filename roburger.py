e1ea0d7a6ce0ec32b912eaf6640ddae92f88e019
jfyuen <jfyuen@gmail.com>
2015-11-10 14:06:25 +0100

    Refactor a browser class, PY3K compatible + remove dependency on lxml

--- roburger.py
+++ roburger.py
@@ -1,13 +1,13 @@
-from collections import defaultdict
 import random
-import time
 import ssl
+from collections import defaultdict
 
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.poolmanager import PoolManager
 from robobrowser import RoboBrowser
 
 import config
+
 
 def max_radio_map(brow):
     radio_map = defaultdict(list)
@@ -21,53 +21,72 @@ def max_radio_map(brow):
 
     return radio_map
 
+
 # Custom SSL Adapter, see:
 # https://lukasa.co.uk/2013/01/Choosing_SSL_Version_In_Requests/   
-class MyAdapter(HTTPAdapter):
+class HTTPSAdapter(HTTPAdapter):
     def init_poolmanager(self, connections, maxsize, block=False):
-        self.poolmanager = PoolManager( num_pools=connections,
-                                        maxsize=maxsize,
-                                        block=block,
-                                        ssl_version=ssl.PROTOCOL_TLSv1)
+        self.poolmanager = PoolManager(num_pools=connections,
+                                       maxsize=maxsize,
+                                       block=block,
+                                       ssl_version=ssl.PROTOCOL_TLSv1)
 
-# Browse url :
-browser = RoboBrowser(parser="lxml")
-browser.session.headers = config.headers
 
-# Mount with custom SSL Adapter
-browser.session.mount('https://', MyAdapter())
+class BKBrowser(object):
+    def __init__(self):
+        # Browse url :
+        self.browser = RoboBrowser(parser="html.parser")
+        self.browser.session.headers = config.headers
+        # Mount with custom SSL Adapter
+        self.browser.session.mount('https://', HTTPSAdapter())
 
-# Get to website
-print "- Connecting to url ..."
-browser.open(config.url)
+    def _connect(self):
+        # Get to website
+        print("- Connecting to url ...")
+        self.browser.open(config.url)
 
-# Click on first button to go to second page:
-button = browser.get_forms()[0]
-browser.submit_form(button)
+    def _skip_first_page(self):
+        button = self.browser.get_forms()[0]
+        self.browser.submit_form(button)
 
-# Fill in Date/Time form and start the Questionnaire
-form = browser.get_forms()[0]
-form['JavaScriptEnabled'].value = '1'
-form['SurveyCode'].value = config.ID
-form['InputMonth'].value = config.date[0]
-form['InputDay'].value = config.date[1]
-form['InputHour'].value = config.time[0]
-form['InputMinute'].value = config.time[1]
+    # Let's fill in the proper form !
+    def _fill_form(self):
+        while not self.browser.find('p', {'class': 'ValCode'}):
+            inputs_map = max_radio_map(self.browser)
+            f = self.browser.get_forms()[0]
+            for i in f.keys():
+                if f[i].value == '':
+                    answers_list = inputs_map.get(i, ['1'])
+                    f[i].value = random.choice(answers_list)
+            f.serialize()
+            self.browser.submit_form(f)
 
-form.serialize()
-browser.submit_form(form)
+    def _fill_date_form(self):
+        # Fill in Date/Time form and start the Questionnaire
+        print("- Filling Forms Randomly ...")
+        form = self.browser.get_forms()[0]
+        form['JavaScriptEnabled'].value = '1'
+        form['SurveyCode'].value = config.ID
+        form['InputMonth'].value = config.date[0]
+        form['InputDay'].value = config.date[1]
+        form['InputHour'].value = config.time[0]
+        form['InputMinute'].value = config.time[1]
+        form.serialize()
+        self.browser.submit_form(form)
 
-print "- Filling Forms Randomly ..."
-# Let's fill in the proper questionnaire !
-while not browser.find('p', {'class': 'ValCode'}):
-    inputs_map = max_radio_map(browser)
-    f = browser.get_forms()[0]
-    for i in f.keys():
-        if f[i].value == '':
-            answers_list = inputs_map.get(i, ['1'])
-            f[i].value = random.choice(answers_list)
-    f.serialize()
-    browser.submit_form(f)
+    def get_validation_code(self):
+        self._connect()
+        self._skip_first_page()
+        self._fill_date_form()
+        self._fill_form()
+        return self.browser.find('p', {'class': 'ValCode'}).text
 
-print "- " + browser.find('p', {'class': 'ValCode'}).text
-print "- Bon appetit !"
+
+def main():
+    browser = BKBrowser()
+    print("- " + browser.get_validation_code())
+    print("- Bon appetit !")
+
+
+if __name__ == '__main__':
+    main()
